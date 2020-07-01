81e0fd32076c3f0cf4b441a9761649930a1d279d
vagrant <vagrant@localhost.localdomain>
2020-07-01 05:45:18 +0000

    [Finish]Task1

--- bookers1-debug/app/controllers/books_controller.rb
+++ bookers1-debug/app/controllers/books_controller.rb
@@ -1,6 +1,7 @@
 class BooksController < ApplicationController
   before_action :set_book, only: [:show, :edit, :update, :destroy]
 
+
   # GET /books
   # GET /books.json
   def index
@@ -15,11 +16,13 @@ class BooksController < ApplicationController
 
   # GET /books/1/edit
   def edit
+    @book = Book.find(params[:id])
   end
 
   # POST /books
   # POST /books.json
   def create
+    @books = Book.all
     @book = Book.new(book_params)
     if @book.save
       redirect_to @book, notice: 'Book was successfully created.'
@@ -31,8 +34,9 @@ class BooksController < ApplicationController
   # PATCH/PUT /books/1
   # PATCH/PUT /books/1.json
   def update
-    if @book.update()
-      redirect_to @book, notice: 'Book was successfully updated.'
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book), notice: 'Book was successfully updated.'
     else
       render :edit
     end
