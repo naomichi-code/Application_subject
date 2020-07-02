db05076cb44a3717ac675c47e8df2fe192858038
vagrant <vagrant@localhost.localdomain>
2020-07-02 11:38:19 +0000

    add book_comment

--- bk2-before_debug/app/controllers/books_controller.rb
+++ bk2-before_debug/app/controllers/books_controller.rb
@@ -5,11 +5,13 @@ class BooksController < ApplicationController
   def show
     @book_new = Book.new
     @book = Book.find(params[:id])
+    @book_comment = BookComment.new
   end
 
   def index
     @book = Book.new
     @books = Book.all
+
   end
 
   def create
--- bk2-before_debug/config/routes.rb
+++ bk2-before_debug/config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
-  get 'favorites/create'
-  get 'favorites/destroy'
-  get 'book_comments/create'
-  get 'book_comments/destroy'
-  devise_for :users, controllers: {
-    sessions: 'users/sessions',
-    registrations: 'users/registrations'
-  }
-  root 'home#top'
-  get 'home/about' => 'home#about'
-  resources :users,only: [:show,:index,:edit,:update]
-  resources :books
+    devise_for :users, controllers: {
+      sessions: 'users/sessions',
+      registrations: 'users/registrations'
+    }
+    root 'home#top'
+    get 'home/about' => 'home#about'
+    resources :users,only: [:show,:index,:edit,:update]
+    resources :books do
+      resource :favorites, only: [:create, :destroy]
+      resources :book_comments, only: [:create, :destroy]
+    end
   end
