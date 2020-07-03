f68dfb8a3926436408afa5cde0d8253e614e3ace
vagrant <vagrant@localhost.localdomain>
2020-07-03 16:04:53 +0000

    add_following_followers

--- bk2-before_debug/config/routes.rb
+++ bk2-before_debug/config/routes.rb
@@ -5,7 +5,11 @@ Rails.application.routes.draw do
     }
     root 'home#top'
     get 'home/about' => 'home#about'
-    resources :users,only: [:show,:index,:edit,:update]
+    resources :users,only: [:show,:index,:edit,:update]do
+    member do
+      get :following, :followers
+      end
+    end
     resources :books do
       resource :favorites, only: [:create, :destroy]
       resources :book_comments, only: [:create, :destroy]
