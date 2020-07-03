09bd770f7ecb9ae898b57653e71e9c5bc47ef5cb
vagrant <vagrant@localhost.localdomain>
2020-07-03 14:08:20 +0000

    add follow_btn

--- bk2-before_debug/app/models/user.rb
+++ bk2-before_debug/app/models/user.rb
@@ -1,16 +1,37 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+    devise :database_authenticatable, :registerable,
+           :recoverable, :rememberable, :validatable
 
-  has_many :books, dependent: :destroy
-  has_many :favorites, dependent: :destroy
-  has_many :book_comments, dependent: :destroy
-  attachment :profile_image
+    has_many :books, dependent: :destroy
+    has_many :favorites, dependent: :destroy
+    has_many :book_comments, dependent: :destroy
+    attachment :profile_image
+  #フォロー機能
+  has_many :relationships #has_many :relationships, foreign_key: 'user_id'と一緒の意味
+  has_many :followings, through: :relationships, source: :follow #フォローしているユーザー達を取得
+  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id' #フォローされているユーザー達を取得
+  has_many :followers, through: :reverse_of_relationships, source: :user
 
-  #バリデーションは該当するモデルに設定する。エラーにする条件を設定できる。
-  validates :name, presence: true
-  validates :name, length: {maximum: 20, minimum: 2}
-  validates :introduction,length:{maximum: 50}
+
+    #バリデーションは該当するモデルに設定する。エラーにする条件を設定できる。
+    validates :name, presence: true
+    validates :name, length: {maximum: 20, minimum: 2}
+    validates :introduction,length:{maximum: 50}
+
+  def follow(other_user)
+    unless self == other_user
+      self.relationships.find_or_create_by(follow_id: other_user.id)
+    end
+  end
+
+  def unfollow(other_user)
+    relationship = self.relationships.find_by(follow_id: other_user.id)
+    relationship.destroy if relationship
+  end
+
+  def following?(other_user)
+    self.followings.include?(other_user)
+  end
 end
--- bk2-before_debug/config/routes.rb
+++ bk2-before_debug/config/routes.rb
@@ -10,5 +10,6 @@ Rails.application.routes.draw do
       resource :favorites, only: [:create, :destroy]
       resources :book_comments, only: [:create, :destroy]
     end
+      resources :relationships, only: [:create, :destroy]
 
   end
