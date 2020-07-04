a23862153bed7442dd1f7ab83986749486dc6855
vagrant <vagrant@localhost.localdomain>
2020-07-04 16:04:50 +0000

    [Fix]Task4

--- bk2-before_debug/app/controllers/searches_controller.rb
+++ bk2-before_debug/app/controllers/searches_controller.rb
@@ -4,6 +4,8 @@ class SearchesController < ApplicationController
         @range = params[:range]
         search = params[:search]
         word = params[:word]
+        @word = params[:word]
+        
 
         if @range == '1'
         @user = User.search(search,word)
